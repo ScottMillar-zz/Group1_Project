477a2ab42604604d3e97f8edf3086b47e05b48d6
Kieran Lowe <Kieran-Lowe@users.noreply.github.com>
2016-05-02 18:09:41 +0100

    Major Changes

--- ConsoleBot/Program.cs
+++ ConsoleBot/Program.cs
@@ -76,11 +76,11 @@ namespace ConsoleBot
                     Result res = myBot.Chat(r);
 
                     //Used to pretend that the mentalist is reading the message before showing 'The Mentalist is writing...'
-                    Thread.Sleep(userInput.Length * rand.Next(150, 300));
+                    //Thread.Sleep(userInput.Length * rand.Next(150, 300));
                     Console.WriteLine("The Mentalist is writing... ");
 
                     output = res.Output;
-                    Thread.Sleep(output.Length * rand.Next(80, 150));
+                    //Thread.Sleep(output.Length * rand.Next(80, 150));
 
                     Console.SetCursorPosition(0, Console.CursorTop - 1);
                     clearCurrentConsoleLine();
